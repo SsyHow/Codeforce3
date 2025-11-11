7ab9784460d64a82eee433ddf2d519dcdd1e52e2
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-11-11 21:33:15 +1100

    Add files via upload

---
+++ 624b/main.py
@@ -0,0 +1,35 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a = II()
+L = LII()
+L.sort(reverse=True)
+
+ans = 0
+f = 1 << 60
+for i in L:
+    ans += min(f - 1, i)
+    f = max(1, min(f - 1, i))
+print(ans)
