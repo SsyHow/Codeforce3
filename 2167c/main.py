820e4b465bcea3359c0fdcb91e693eb9a438de89
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-11-14 22:57:23 +1100

    Add files via upload

---
+++ 2167c/main.py
@@ -0,0 +1,42 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a = II()
+for _ in range(a):
+    b = II()
+    L = LII()
+
+    odd, even = False, False
+
+    for i in L:
+        if i & 1 == 0:
+            even = True
+        else:
+            odd = True
+
+    if odd and even:
+        print(*sorted(L))
+    else:
+        print(*L)
