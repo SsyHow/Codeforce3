f70716a7b1ac1554c87371dbecc5ca0f7c083ee3
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-11-14 22:57:13 +1100

    Add files via upload

---
+++ 2167a/main.py
@@ -0,0 +1,33 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+k = II()
+for _ in range(k):
+    a, b, c, d = MII()
+    if a == b == c == d:
+        print("YES")
+    else:
+        print("NO")
