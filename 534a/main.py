5f4742a9a3271332f8f216d908735ca62d90def1
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-11-16 22:53:15 +1100

    Add files via upload

---
+++ 534a/main.py
@@ -0,0 +1,36 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a = II()
+if a == 3:
+    print(2)
+    print(1, 3)
+elif a == 2:
+    print(1)
+    print(1)
+else:
+    print(a)
+    print(*range(2, a + 1, 2), *range(1, a + 1, 2))
+
