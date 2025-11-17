376d5e45a8007ba74b79426dc645126226f3700e
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-11-17 21:08:30 +1100

    Add files via upload

---
+++ 987b/main.py
@@ -0,0 +1,32 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+from math import log, e
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+x, y = MII()
+if x * log(y, e) == y * log(x, e):
+    print("=")
+elif x * log(y, e) < y * log(x, e):
+    print(">")
+else:
+    print("<")
