c2ab687cbdba3e110edbdb9e82e2605822492103
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-12-15 17:07:06 +0900

    Add files via upload

---
+++ 1121b/main.py
@@ -0,0 +1,33 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+from collections import defaultdict
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a = II()
+L = LII()
+d = defaultdict(int)
+for i in range(a - 1):
+    for j in range(i + 1, a):
+
+        d[L[i] + L[j]] += 1
+print(sorted(d.items(), key = lambda x: -x[1])[0][1])
